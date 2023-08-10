49160e7fbdfc25fe5ba7856b3c954fdbdf21deed
KnackPR <TailBeast113@gmail.com>
2023-08-10 15:38:58 +0700

    เย้3

--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -4,7 +4,7 @@ export const PostOwner = (props) => {
       <div className="vstack gap-3">
         <div className="d-flex align-items-center gap-3">
           <img
-            src="/profileImages/IMG_1029.jpg"
+            src="/profileImages/Knack.jpg"
             width="48"
             height="48"
             className="rounded-circle"
